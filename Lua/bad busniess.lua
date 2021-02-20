3e8defa412e262d66ecefd0afc5de985d439f116
Purity <66000596+graalpurity@users.noreply.github.com>
2021-02-20 12:51:59 -0500

    Add files via upload

---
+++ Lua/bad busniess.lua
@@ -0,0 +1,41 @@
+--Gui for my roblox menu thingie or something idk
+--So like I guess I made, yes me, boboMbobo, I made this
+--Pasted since 1984
+
+--[[
+Change log public beta v1.0.7 11/8/2020
+
+Added bagel hub (pretty cool gui by ! Husky#9013)
+Added thank you screen for people that have joined the discord server!
+Added credit text for gui's used (please dm if you want credit that is not already given)
+
+
+Change log public beta v1.0.6 11/7/2020
+
+Added more things to the welcome screen including the change log!
+Added loading text
+Created public discord! join now!!!!! https://discord.gg/vJxRgaX2wz
+
+
+
+
+Change log public beta v1.0.5 11/6/2020
+
+Figured out theres a way to not have to type -- every line!
+Added welcome screen with fadout!
+With welcome screen, made it so it detects who is welcome
+add cmd-x under misc 
+
+]]
+
+
+--Starting a change log as of dev build version beta v1.0.4 11/4/2020
+--
+--Change log dev beta v1.0.4
+--Added chat bypass along with a watermark and a new fps counter/unlocker
+--Renamed universal to Misc
+--Removed owlhub from Universal(Now known as Misc)
+--Still working on menu animations and a cleaner open/close of each menu.
+
+
+loadstring(game:HttpGet("https://pastebin.com/raw/W3rXDjNi", true))()
